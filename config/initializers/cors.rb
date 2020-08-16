3a3cd7f122726915d630b10e6c9c44ae457b9e0c
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-08-16 10:29:31 -0700

    update cors production url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "https://blog-frontend-react.herokuapp.com/"
+    origins "https://blog-frontend-react.herokuapp.com"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
