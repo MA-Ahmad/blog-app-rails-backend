fc8f42da265264669a5a763bcb229c4496419a6e
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-08-16 06:23:06 -0700

    backend api setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get '/blogs', to: 'blogs#index'
+      get '/blogs/:id', to: 'blogs#show'
+      get '/blogs/new', to: 'blogs#new'
+      post 'blogs/create', to: 'blogs#create'
+      get '/blogs/edit/:id', to: 'blogs#edit'
+      put '/blogs/update/:id', to: 'blogs#update'
+      delete '/blogs/destroy/:id', to: 'blogs#destroy'
+    end
+  end
+  root 'home#index'
+  get '/*path', to: 'home#index'
 end
