eaa1e37233dc6c5f85fdd4dfa57d3ae809c1b452
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-10-07 01:11:45 -0700

    Update authable.rb

--- app/controllers/concerns/authable.rb
+++ app/controllers/concerns/authable.rb
@@ -11,7 +11,7 @@ module Authable
     @current_user ||= User.find(session[:user_id])
   end
 
-  # def authenticate
-  #   render json: { error: 'Access Denied' }, status: 401 unless current_user
-  # end
+  def authenticate
+    render json: { error: 'Access Denied' }, status: 401 unless current_user
+  end
 end
