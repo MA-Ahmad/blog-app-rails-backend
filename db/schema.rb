8e1eadffb9ea49ba9af238008c3af48ef0eb0f6a
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-10-07 01:11:37 -0700

    add name column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_05_161736) do
+ActiveRecord::Schema.define(version: 2020_10_06_164434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_10_05_161736) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   add_foreign_key "blogs", "users"
