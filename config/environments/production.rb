f6678a0ddeabe52f600ae02e8945f9ef1e9b7b25
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-10-08 04:54:35 -0700

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,8 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  routes.default_url_options[:host] = 'https://blog-backend-rails.herokuapp.com'
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
