d817cde6c24c01be39e8d204d48e82c6d9c7e383
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-10-08 04:46:08 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/blogs', to: 'blogs#index'
-      get '/blogs/:id', to: 'blogs#show'
-      get '/blogs/new', to: 'blogs#new'
-      post 'blogs/create', to: 'blogs#create'
-      get '/blogs/edit/:id', to: 'blogs#edit'
-      put '/blogs/update/:id', to: 'blogs#update'
-      delete '/blogs/destroy/:id', to: 'blogs#destroy'
-
+      resources :blogs
       resources :sessions, only: %i[create] do
         collection do
           delete :logout, to: 'sessions#logout'
@@ -16,9 +9,10 @@ Rails.application.routes.draw do
         end
       end
       resources :registrations, only: %i[create]
+      resources :users, only: %i[update]
     end
   end
 
   root 'home#index'
-  get '*path', to: 'pages#index', via: :all
+  # get '*path', to: 'pages#index', via: :all
 end
