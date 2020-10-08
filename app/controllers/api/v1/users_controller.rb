4e46d10095fdaf0ea5221ed5b75dd014e1ee7341
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-10-08 04:47:26 -0700

    Create users_controller.rb

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::UsersController < ApiController
+    before_action :authenticate, only: :update
+    
+    def update
+        if current_user.update(JSON.parse(params[:user]))
+            current_user.attach_image(params[:image]) if params[:image].present?
+            render json: @current_user
+        else
+            render json: { status: :error, updated: false, }, status: 422
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :image_url)
+    end
+end
+  
