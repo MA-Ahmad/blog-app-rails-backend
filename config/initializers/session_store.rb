7a28c975694f8be1dd6938d3ef4c6b908a6e9e0e
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-10-08 13:55:06 -0700

    Update session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,7 @@
 if Rails.env == 'production'
     # Rails.application.config.session_store :cookie_store, key: "_blog_backend", domain: "https://blog-backend-rails.herokuapp.com"
     # Rails.application.config.session_store :cookie_store, :key => '_blog_backend' , :domain => '.blog-backend-rails.herokuapp.com'
+    Rails.application.config.session_store :cookie_store, :key => '_blog_backend' , domain: :all
 else
     Rails.application.config.session_store :cookie_store, key: "_blog_backend", domain: "localhost"
 end
