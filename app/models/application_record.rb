03ebbad5e3c7d9d81a867674bb0f73ec099ad32a
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-10-08 04:47:20 -0700

    Update application_record.rb

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
+  include Rails.application.routes.url_helpers
   self.abstract_class = true
+
+  def attach_image image_file
+    self.image.attach(image_file)
+    self.update(image_url: polymorphic_url(self.image))
+  end
 end
