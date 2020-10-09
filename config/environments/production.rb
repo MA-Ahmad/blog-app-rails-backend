9643d08c78fc68d68157a1c012ae1c66b3c007ce
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-10-09 07:57:29 -0700

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,12 +12,9 @@ Rails.application.configure do
 
   routes.default_url_options[:host] = 'https://blog-backend-rails.herokuapp.com'
 
-  # config.session_store :cookie_store, :key => '_blog_backend', :domain => :all
-  # config.session_store :cookie_store, :key => '_blog_backend' , :domain => '.blog-backend-rails.herokuapp.com'
-
-  config.serve_static_assets = true
+  # config.serve_static_assets = true
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
