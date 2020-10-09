3ab512c5c22fbe5a64e92b099f234a80b9eb50ca
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-10-09 14:34:34 -0700

    Update users_controller.rb

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,8 @@ class Api::V1::UsersController < ApiController
     
     def update
         if current_user.update(JSON.parse(params[:user]))
-            current_user.attach_image(params[:image]) if params[:image].present?
+            current_user.update(image: params[:image]) if params[:image].present?
+            # current_user.attach_image(params[:image]) if params[:image].present?
             render json: @current_user
         else
             render json: { status: :error, updated: false, }, status: 422
@@ -13,7 +14,7 @@ class Api::V1::UsersController < ApiController
     private
 
     def user_params
-        params.require(:user).permit(:name, :image_url)
+        params.require(:user).permit(:name, :image)
     end
 end
   
