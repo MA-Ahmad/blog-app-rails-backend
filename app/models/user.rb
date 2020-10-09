566f7fa01b5bcc261e7d63411c908ec25b6267c1
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-10-09 14:34:56 -0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true
     
     has_many :blogs
-    has_one_attached :image
+    # has_one_attached :image_file
+    mount_uploader :image, ImageUploader
 end
