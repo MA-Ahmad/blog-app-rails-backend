c714962282d165f1a3fa11f5a3de33a4db6cb1d4
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-10-10 06:39:22 -0700

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module BlogBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
