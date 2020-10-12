2a7d73f37c7f02312fdecd5dd780360ef1df4f4e
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-10-12 10:43:14 -0700

    added strong parameters

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,9 +2,8 @@ class Api::V1::UsersController < ApiController
     before_action :authenticate, only: :update
     
     def update
-        if current_user.update(JSON.parse(params[:user]))
-            # current_user.update(image: params[:image]) if params[:image].present?
-            current_user.attach_image(params[:image]) if params[:image].present?
+        if current_user.update(user_params)
+            current_user.attach_image(params[:user][:image_file]) if params[:user][:image_file].present?
             render json: @current_user
         else
             render json: { status: :error, updated: false, }, status: 422
@@ -14,7 +13,7 @@ class Api::V1::UsersController < ApiController
     private
 
     def user_params
-        params.require(:user).permit(:name, :image)
+        params.require(:user).permit(:name, :image_file)
     end
 end
   
