8710d4a84b2ca459ad61094fc39b7d7ca81f87d4
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-10-19 14:39:54 -0700

    email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
     has_one_attached :image_file
     has_many :visits, class_name: "Ahoy::Visit"
 
-    validates :email, presence: true, uniqueness: true
+    validates :email, with: URI::MailTo::EMAIL_REGEXP, 
+            presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 254 }
 end
