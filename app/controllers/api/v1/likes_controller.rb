2c51390d3fac43f28f57ee1c02d8231e69f59e88
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-11-12 21:53:26 -0800

    Update likes_controller.rb

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::LikesController < ApiController
         params[:like][:user_id] = current_user.id
         @like = Like.new(like_params)
         if @like.save
-        #   count = Blog.find(params[:like][:reference_id]).likes.count if params[:like][:reference_type] == 'Blog'
             set_blogs
             render json: @blogs
         elsif already_liked?
